3d6758cb667efe2624ab218738c125249561c4f3
tmtomer <trevor.tomer@outlook.com>
2021-11-13 17:43:25 -0600

    Adding button for typeform navigation

--- src/questionare.js
+++ src/questionare.js
@@ -1,9 +1,28 @@
 
-import { height } from '@mui/system'
-import { Widget } from '@typeform/embed-react'
+import { Button } from '@mui/material';
+import { Widget } from '@typeform/embed-react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme2 = createTheme({
+    palette: {
+      secondary: {
+        main: '#004879'
+      }
+    }
+  });
 
 function MyComponent() {
-  return <Widget id="xIytYAdP" className="typeform-style" />
+    return (
+    <div>
+        <Widget id="xIytYAdP" className="typeform-style" />
+        <div className="button-style">
+        <ThemeProvider theme={theme2}>
+            <Button variant="contained" color="secondary" size="large">Continue To Dashboard</Button>
+        </ThemeProvider>
+        </div>
+    </div>
+ 
+    );
 }
 
 export default MyComponent

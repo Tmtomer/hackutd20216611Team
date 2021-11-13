8e46527bd45ebd42a918af1a5a27a6f1b0c86772
tmtomer <trevor.tomer@outlook.com>
2021-11-13 16:22:28 -0600

    Adding typeform screen

---
+++ src/questionare.js
@@ -0,0 +1,9 @@
+
+import { height } from '@mui/system'
+import { Widget } from '@typeform/embed-react'
+
+function MyComponent() {
+  return <Widget id="xIytYAdP" className="typeform-style" />
+}
+
+export default MyComponent

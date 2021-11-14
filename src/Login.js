9f9c2a2fb6eef5e6a3b395a8413aa689852ecfd3
Kramer <m233324@academy.usna.edu>
2021-11-13 20:48:26 -0600

    Login page and Dependencies

--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import LockIcon from '@mui/icons-material/Lock';
+import { TextField } from "@material-ui/core";
+import './App.css';
 
-const Login=()=>{
-
-    const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
+function Login(){
+    const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto", backgroundColor:"#b8afae"}
+    const avatarStyle={backgroundColor:'blue'}
     return(
-        <Grid>
-            <Paper elevation ={10} style ={paperStyle}>
+        <div style={{color:'#7a7777'}}>
+            <Grid>
+            <Paper elevation ={10} style ={paperStyle} >
                 <Grid align = 'center'>
-                <Avatar ><LockIcon/></Avatar>
+                <Avatar style={avatarStyle}><LockIcon/></Avatar>
                 <h2>Sign in</h2>
                 </Grid>
+                <TextField label='Username' placeholder='Enter Username' fullWidth required/>
+                <TextField label='Password' placeholder='Enter Password' type='password' fullWidth required/>
+                <Button type= 'submit' color='primary' variant="contained" fullWidthSign>Sign in</Button>
             </Paper>
         </Grid>
+        
+        </div>
     )
 }
 
